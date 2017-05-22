a4c9028cc764dbbe8219c412a47d0bac81ed2d12
PythonCakeMing <1851464029@qq.com>
2017-05-22 20:42:21 +0800

    app

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+from .config import configs
+from .models import db
+
+from .vendors import bcrypt
+from .vendors import login_manager
+
+# from .auth import auth_blueprint
+# from .main import main_blueprint
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(configs[config_name])
+
+    # init vendors here.
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    # register blueprints here.
+    # app.register_blueprint(auth_blueprint)
+    # app.register_blueprint(main_blueprint)
+
+    return app
