4d6730826f0dc45e91e7d3ba2ae6d949e65985de
PythonCakeMing <1851464029@qq.com>
2017-05-23 11:30:54 +0800

    essential models

---
+++ app/models/essentials.py
@@ -0,0 +1,33 @@
+from . import db
+
+
+class Disease(db.Model):
+    name_ch = db.Column('name_ch', db.String(30), nullable=False)
+    name_en = db.Column('name_ch', db.String(30), nullable=False)
+
+
+class Region(db.Model):
+    province = db.Column('province', db.String(30), nullable=False)
+    city = db.Column('city', db.String(30), nullable=False)
+    longitude = db.Column('longitude', db.Float(12, 6), nullable=True)
+    latitude = db.Column('latitude', db.Float(12, 6), nullable=True)
+
+
+class AgeGroup(db.Model):
+    range = db.Column('range', db.String(30), nullable=False)
+    range_description = db.Column('range_description', db.String(30), nullable=False)
+
+
+class HospitalLevel(db.Model):
+    level = db.Column('level', db.String(30), nullable=False)
+    level_description = db.Column('level_description', db.String(30), nullable=False)
+
+
+class HospitalOffice(db.Model):
+    office = db.Column('office', db.String(30), nullable=False)
+    office_description = db.Column('office_description', db.String(30), nullable=False)
+
+
+class DoctorTitle(db.Model):
+    title = db.Column('title', db.String(30), nullable=False)
+    title_description = db.Column('title_description', db.String(30), nullable=False)
