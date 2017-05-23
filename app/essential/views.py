c83fa13522b3b32e5bd63c891f6875036a6ff36c
PythonCakeMing <1851464029@qq.com>
2017-05-23 13:12:28 +0800

    essential statistics

--- app/essential/views.py
+++ app/essential/views.py
@@ -1,6 +1,17 @@
 from flask import url_for
 from flask import redirect
+from flask_login import login_required
 
 from . import essential_blueprint
 
 
+@essential_blueprint.route('/daily_count/')
+@login_required
+def daily_count():
+    pass
+
+
+@essential_blueprint.route('/region_count/')
+@login_required
+def region_count():
+    pass
