eef3940aad4ba123073b1a1a86a3b81f6ffcce57
PythonCakeMing <1851464029@qq.com>
2017-05-23 14:07:08 +0800

    doctor model

--- app/models/doctors.py
+++ app/models/doctors.py
@@ -5,4 +5,18 @@ class Doctor(db.Model):
     __tablename__ = 'doctor'
     id = db.Column(db.Integer(), primary_key=True)
 
-    year = db.Column('year', db.Integer(), nullable=False)
+    name = db.Column('name', db.String(30), nullable=True)
+    real_name = db.Column('real_name', db.String(30), nullable=True)
+    phone = db.Column('phone', db.String(11), nullable=False, unique=True)
+
+    region_id = db.Column(db.Integer(), db.ForeignKey('region.id'))
+    age_group_id = db.Column(db.Integer(), db.ForeignKey('age_group.id'))
+    doctor_title_id = db.Column(db.Integer(), db.ForeignKey('doctor_title.id'))
+    hospital_level_id = db.Column(db.Integer(), db.ForeignKey('hospital_level.id'))
+    hospital_office_id = db.Column(db.Integer(), db.ForeignKey('hospital_office.id'))
+
+    register_year = db.Column('register_year', db.String(30), nullable=True)
+    register_month = db.Column('register_month', db.String(30), nullable=True)
+    register_day = db.Column('register_day', db.String(30), nullable=True)
+
+
