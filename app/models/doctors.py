3144b61f429e1e23fb2d61205311d426325774f9
PythonCakeMing <1851464029@qq.com>
2017-05-24 11:02:36 +0800

    authenticated

--- app/models/doctors.py
+++ app/models/doctors.py
@@ -23,6 +23,7 @@ class Doctor(db.Model):
 class AuthenticatedDoctor(db.Model):
     __tablename__ = 'authenticated_doctor'
     id = db.Column(db.Integer(), primary_key=True)
+    doctor_id = db.Column(db.Integer(), db.ForeignKey('doctor.id'))
     real_name = db.Column('real_name', db.String(30), nullable=False)
     register_year = db.Column('register_year', db.String(30), nullable=False)
     id_number = db.Column('id_number', db.String(30), nullable=False)
