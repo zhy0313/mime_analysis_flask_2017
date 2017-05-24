9850dd102db5308b106b44dcdfba70a329a3f4fd
fishyu <501254061@qq.com>
2017-05-24 23:48:13 +0800

    test

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -5,6 +5,12 @@ var now_year = now_date.getFullYear(),
 
 var color = ['#00a0e9', '#E74C3C', '#34495E', '#E67E22', '#1ABC9C', '#95A5A6', '#9B59B6', '#F1C40F', '#2980B9', '#16A085', '#2C3E50', '#27AE60', '#C0392B', '#7F8C8D', '#D35400', '#8E44AD', '#F39C12'];
 
+// $.ajaxSetup({
+//     headers: {
+//         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//     }
+// });
+
 $(document).ready(function() {
     $('.input-group').on('focus', '.form-control', function() {
         $(this).closest('.input-group, .form-group').addClass('focus');
