50455224e73418fe2749ec7e974bdbbeab98d496
PythonCakeMing <1851464029@qq.com>
2017-05-26 17:34:28 +0800

    query

--- app/blueprint_home/views.py
+++ app/blueprint_home/views.py
@@ -2,20 +2,24 @@ import json
 from flask import render_template
 from flask_login import login_required
 from . import blueprint_home
+from ..models import db
 from ..models import DailyCount
 from ..models import RegionCount
 from sqlalchemy import func
+
 
 @blueprint_home.route('/')
 @blueprint_home.route('/index/')
 #@login_required
 def index():
-    register_count = DailyCount.query(func.sum(DailyCount.register_count))
-    authorize_count = DailyCount.query(func.sum(DailyCount.authorize_count))
-    visit_count = DailyCount.query(func.sum(DailyCount.visit_count))
-    trade_count = DailyCount.query(func.sum(DailyCount.trade_count))
-    ret = {'register_count': register_count, 'authorize_count': authorize_count, 'visit_count': visit_count, 'trade_count': trade_count}
-    return render_template('home.html', statistic_info=ret)
+    register_count = db.session.query(func.sum(DailyCount.register_count)).all()
+    print(register_count)
+    # register_count = DailyCount.query(func.sum(DailyCount.register_count))
+    # authorize_count = DailyCount.query(func.sum(DailyCount.authorize_count))
+    # visit_count = DailyCount.query(func.sum(DailyCount.visit_count))
+    # trade_count = DailyCount.query(func.sum(DailyCount.trade_count))
+    # ret = {'register_count': register_count, 'authorize_count': authorize_count, 'visit_count': visit_count, 'trade_count': trade_count}
+    return render_template('home.html')
 
 
 @blueprint_home.route('/daily_count/')
