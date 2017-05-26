94db2e057dd17717532a9b845df7869d9b50cb28
boyaxxx <xiaolu625@gmail.com>
2017-05-26 15:52:36 +0800

    首页统计信息

--- app/blueprint_home/views.py
+++ app/blueprint_home/views.py
@@ -4,13 +4,18 @@ from flask_login import login_required
 from . import blueprint_home
 from ..models import DailyCount
 from ..models import RegionCount
-
+from sqlalchemy import func
 
 @blueprint_home.route('/')
 @blueprint_home.route('/index/')
-@login_required
+#@login_required
 def index():
-    return render_template('home.html')
+    register_count = DailyCount.query(func.sum(DailyCount.register_count))
+    authorize_count = DailyCount.query(func.sum(DailyCount.authorize_count))
+    visit_count = DailyCount.query(func.sum(DailyCount.visit_count))
+    trade_count = DailyCount.query(func.sum(DailyCount.trade_count))
+    ret = {'register_count': register_count, 'authorize_count': authorize_count, 'visit_count': visit_count, 'trade_count': trade_count}
+    return render_template('home.html', statistic_info=ret)
 
 
 @blueprint_home.route('/daily_count/')
