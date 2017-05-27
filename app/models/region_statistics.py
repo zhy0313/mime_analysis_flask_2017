cd7d96a3a1e580784baa4e19d689cf30b70dbb1d
PythonCakeMing <1851464029@qq.com>
2017-05-27 10:49:58 +0800

    models

--- app/models/region_statistics.py
+++ app/models/region_statistics.py
@@ -10,7 +10,6 @@ class RegionCount(db.Model):
     month = db.Column('month', db.Integer(), nullable=False)
     day = db.Column('day', db.Integer(), nullable=False)
 
-    visit_count = db.Column('visit_count', db.Integer(), nullable=False)
     register_count = db.Column('register_count', db.Integer(), nullable=False)
     authorize_count = db.Column('authorize_count', db.Integer(), nullable=False)
     trade_count = db.Column('trade_count', db.Integer(), nullable=False)
